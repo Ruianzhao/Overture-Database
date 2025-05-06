cb183a22ecef9dad16c74637c7c6261d544000b7
Ruian Zhao <ruianzhao@gmail.com>
2025-05-06 17:21:17 -0400

    Utilized Counter to make counting the amount of times each category appears more efficient

--- initialTable.py
+++ initialTable.py
@@ -3,34 +3,29 @@ import pandas as pd
 
 import array
 
+from collections import Counter
+
 #Load the parquet file
 toronto_places = pd.read_parquet('toronto_places.parquet', engine='auto')
 
 #Prints the transposed version of the table which allows for better printing
 print(toronto_places.head().T)
+ 
+# Create a list to store all the categories
+categories = []
 
-categoryNames = []
-categoryCount = []
-
-#This loop iterates through every place in the parquet and checks what category they are        
+#This loop goes through every place in the parquet and determines what category they are
 for curr in toronto_places['categories']:
-    #This checks to make sure the dictionary isn't empty and that the primary tag exists
-    if isinstance(curr, dict) and 'primary' in curr:
-        #The value of the primary tag is the type of category the place is
-        primary_cat = curr['primary']
-        #If the category was already is category names, we just have to increment the count for that category
-        if primary_cat in categoryNames:
-            idx = categoryNames.index(primary_cat)
-            categoryCount[idx] += 1
-        else:
-            #Otherwise we add it to the categoryNames and initialize its count to 1
-            categoryNames.append(primary_cat)
-            categoryCount.append(1)
-    
-#Then we convert the two arrays into an array of tuples for easier printing
-for name, count in zip(categoryNames, categoryCount):
-    print(f"{name}: {count}")   
-    
-    
+    #If the dictionary isnt empty, and the primary tag exists, then add the category
+    # into the categories list
+    if isinstance(curr, dict) and 'primary' in  curr:
+        categories.append(curr['primary'])
 
-    
+#Automatically counts the amount of times each category appears in the list
+#And creates a dictionary that contains the category name and appearance amount
+categoryAndCount = Counter(categories)
+
+#Prints out the dictionary line by line
+for category, count in categoryAndCount.items():
+    print(f"{category}: {count}")
+        
